32e2459784243e89ef46d4b6526a0e1e484ed6b9
Ben Scheirman <ben@scheirman.com>
2017-01-30 20:57:37 -0600

    Initial project, welcome screen

---
+++ FiveCalls/FiveCalls/WelcomeViewController.swift
@@ -0,0 +1,79 @@
+//
+//  ViewController.swift
+//  FiveCalls
+//
+//  Created by Ben Scheirman on 1/30/17.
+//  Copyright © 2017 5calls. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+    
+    @IBOutlet weak var pageControl: UIPageControl!
+    @IBOutlet weak var pageContainer: UIView!
+    
+    lazy var pageController: UIPageViewController = {
+        let pageController = UIPageViewController(transitionStyle: .scroll, navigationOrientation: .horizontal, options: nil)
+        pageController.view.translatesAutoresizingMaskIntoConstraints = false
+        return pageController
+    }()
+    
+    let numberOfPages = 2
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        loadPages()
+    }
+
+    private func loadPages() {
+        pageContainer.backgroundColor = .white
+        addChildViewController(pageController)
+        pageContainer.addSubview(pageController.view)
+        
+        NSLayoutConstraint.activate([
+            pageController.view.topAnchor.constraint(equalTo: pageContainer.topAnchor),
+            pageController.view.leftAnchor.constraint(equalTo: pageContainer.leftAnchor),
+            pageController.view.rightAnchor.constraint(equalTo: pageContainer.rightAnchor),
+            pageController.view.bottomAnchor.constraint(equalTo: pageContainer.bottomAnchor),
+            ])
+        pageController.didMove(toParentViewController: self)
+        pageController.dataSource = self
+        pageController.setViewControllers([viewController(atIndex: 0)], direction: .forward, animated: false, completion: nil)
+    }
+}
+
+extension WelcomeViewController : UIPageViewControllerDataSource {
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        let index = viewController.view.tag + 1
+        guard index < numberOfPages else { return nil }
+        return self.viewController(atIndex: index)
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        let index = viewController.view.tag - 1
+        guard index > 0 else { return nil }
+        return self.viewController(atIndex: index)
+    }
+    
+    func viewController(atIndex index: Int) -> UIViewController {
+        guard let storyboard = self.storyboard else { fatalError() }
+        let page = storyboard.instantiateViewController(withIdentifier: "Page\(index + 1)")
+        page.view.tag = index
+        return page
+    }
+    
+    func presentationCount(for pageViewController: UIPageViewController) -> Int {
+        return numberOfPages
+    }
+    
+    func presentationIndex(for pageViewController: UIPageViewController) -> Int {
+        return 0
+    }
+}
+
