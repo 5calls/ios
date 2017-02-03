df9dc3e7bb4a57bb93951288dcf33509a16ab4f7
Ben Scheirman <ben@scheirman.com>
2017-02-03 08:17:38 -0600

    Issue Detail, show title, reason, button to set location

--- FiveCalls/FiveCalls/IssueDetailViewController.swift
+++ FiveCalls/FiveCalls/IssueDetailViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreLocation
 
 class IssueDetailViewController : UIViewController {
     
@@ -14,6 +15,10 @@ class IssueDetailViewController : UIViewController {
     var issue: Issue!
     
     @IBOutlet weak var tableView: UITableView!
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
@@ -23,8 +28,19 @@ class IssueDetailViewController : UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        navigationController?.navigationBar.tintColor = .white
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: nil, action: nil)
+        
         tableView.estimatedRowHeight = 100
         tableView.rowHeight = UITableViewAutomaticDimension
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let nav = segue.destination as? UINavigationController,
+            let loc = nav.viewControllers.first as? EditLocationViewController {
+            loc.delegate = self
+        }
     }
 }
 
@@ -49,7 +65,7 @@ extension IssueDetailViewController : UITableViewDataSource {
         if section == Sections.header.rawValue {
             return HeaderRows.count.rawValue
         } else {
-            return issue.contacts.count
+            return max(1, issue.contacts.count)
         }
     }
     
@@ -59,6 +75,14 @@ extension IssueDetailViewController : UITableViewDataSource {
         case Sections.header.rawValue:
             return headerCell(at: indexPath)
         default:
+            
+            if issue.contacts.isEmpty {
+                let cell = tableView.dequeueReusableCell(withIdentifier: "setLocationCell", for: indexPath)
+                return cell
+            } else {
+                // contact cell
+            }
+            
             return UITableViewCell()
         }
     }
@@ -66,9 +90,15 @@ extension IssueDetailViewController : UITableViewDataSource {
     private func headerCell(at indexPath: IndexPath) -> UITableViewCell {
         switch indexPath.row {
         case HeaderRows.title.rawValue:
-            return tableView.dequeueReusableCell(withIdentifier: "titleCell", for: indexPath)
+            let cell = tableView.dequeueReusableCell(withIdentifier: "titleCell", for: indexPath) as! IssueDetailCell
+            cell.issueLabel.text = issue.name
+            return cell
+            
         case HeaderRows.description.rawValue:
-            return tableView.dequeueReusableCell(withIdentifier: "descriptionCell", for: indexPath)
+            let cell = tableView.dequeueReusableCell(withIdentifier: "descriptionCell", for: indexPath) as! IssueDetailCell
+            cell.issueLabel.text = issue.reason
+            return cell
+            
         default:
             return UITableViewCell()
         }
@@ -77,4 +107,25 @@ extension IssueDetailViewController : UITableViewDataSource {
 
 extension IssueDetailViewController : UITableViewDelegate {
     
+}
+
+extension IssueDetailViewController : EditLocationViewControllerDelegate {
+    func editLocationViewControllerDidCancel(_ vc: EditLocationViewController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func editLocationViewController(_ vc: EditLocationViewController, didSelectZipCode zip: String) {
+        dismiss(animated: true, completion: nil)
+        issuesManager.zipCode = zip
+        issuesManager.fetchIssues {
+            self.tableView.reloadData()
+        }
+    }
+    
+    func editLocationViewController(_ vc: EditLocationViewController, didSelectLocation location: CLLocationCoordinate2D) {
+        dismiss(animated: true, completion: nil)
+        issuesManager.fetchIssues {
+            self.tableView.reloadData()
+        }
+    }
 }
