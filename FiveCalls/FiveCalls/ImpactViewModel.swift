b24a95b71384499ad2f40bea213d909fe206e79e
Ben Scheirman <ben@scheirman.com>
2017-02-06 23:17:22 -0600

    Initial My Impact screen

---
+++ FiveCalls/FiveCalls/ImpactViewModel.swift
@@ -0,0 +1,34 @@
+//
+//  ImpactViewModel.swift
+//  FiveCalls
+//
+//  Created by Ben Scheirman on 2/6/17.
+//  Copyright © 2017 5calls. All rights reserved.
+//
+
+import Foundation
+
+struct ImpactViewModel {
+    let logs: [ContactLog]
+    
+    init(logs: [ContactLog]) {
+        self.logs = logs
+        print(logs.map { $0.outcome })
+    }
+    
+    var numberOfCalls: Int {
+        return logs.count
+    }
+    
+    var madeContactCount: Int {
+        return logs.filter { $0.outcome == "contacted" }.count
+    }
+    
+    var unavailableCount: Int {
+        return logs.filter { $0.outcome == "unavailable" }.count
+    }
+    
+    var voicemailCount: Int {
+        return logs.filter { $0.outcome == "voicemail" || $0.outcome == "vm" }.count
+    }
+}
---
+++ FiveCalls/FiveCalls/MyImpactViewController.swift
@@ -0,0 +1,112 @@
+//
+//  MyImpactViewController.swift
+//  FiveCalls
+//
+//  Created by Ben Scheirman on 2/6/17.
+//  Copyright © 2017 5calls. All rights reserved.
+//
+
+import UIKit
+
+class MyImpactViewController : UITableViewController {
+    
+    var viewModel: ImpactViewModel!
+    
+    @IBOutlet weak var headerLabel: UILabel!
+    @IBOutlet weak var subheadLabel: UILabel!
+    
+    enum Sections: Int {
+        case stats
+        case contacts
+        case count
+        
+        var cellIdentifier: String {
+            switch self {
+            case .stats: return "statCell"
+            case .contacts: return "contactStatCell"
+            default: return ""
+            }
+        }
+    }
+    
+    enum StatRow: Int {
+        case madeContact
+        case voicemail
+        case unavailable
+        case count
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        viewModel = ImpactViewModel(logs: ContactLogs.load().all)
+        
+        let number = viewModel.numberOfCalls
+        let calls = number == 1 ? "call" : "calls"
+        let punctuation = number > 0  ? "!" : "."
+        let template = headerLabel.text
+        let headerString = template?.replacingOccurrences(of: "{{number}}", with: String(number))
+                                    .replacingOccurrences(of: "{{calls}}", with: calls)
+                                    .replacingOccurrences(of: "{{punctuation}}", with: punctuation)
+        headerLabel.text = headerString
+        
+        subheadLabel.isHidden = number == 0
+        
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return Sections.count.rawValue
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        switch Sections(rawValue: section)! {
+        case .stats:
+            return StatRow.count.rawValue
+            
+        case .contacts:
+            return 0
+            
+        default: return 0
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let section = Sections(rawValue: indexPath.section)!
+        let identifier = section.cellIdentifier
+        let cell = tableView.dequeueReusableCell(withIdentifier: identifier, for: indexPath)
+        
+        switch section {
+        case .stats:
+            configureStatRow(cell: cell, stat: StatRow(rawValue: indexPath.row)!)
+        default: break
+        }
+        
+        return cell
+    }
+    
+    private func configureStatRow(cell: UITableViewCell, stat: StatRow) {
+        switch stat {
+        case .madeContact:
+            cell.textLabel?.text = "Made Contact"
+            cell.detailTextLabel?.text = timesString(count: viewModel.madeContactCount)
+        case .unavailable:
+            cell.textLabel?.text = "Unavailable"
+            cell.detailTextLabel?.text = timesString(count: viewModel.unavailableCount)
+        case .voicemail:
+            cell.textLabel?.text = "Left Voicemail"
+            cell.detailTextLabel?.text = timesString(count: viewModel.voicemailCount)
+        default: break
+        }
+    }
+    
+    @IBAction func done(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    private func timesString(count: Int) -> String {
+        return "\(count) time\( count == 1 ? "" : "s")"
+    }
+}
