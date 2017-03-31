b3f8c990dc711ea7923cdfca8e06ff2e14113d6e
Ben Scheirman <ben@scheirman.com>
2017-03-31 16:25:36 -0500

    Location Button, rounded border, highlight styles

---
+++ FiveCalls/FiveCalls/LocationButton.swift
@@ -0,0 +1,34 @@
+//
+//  LocationButton.swift
+//  FiveCalls
+//
+//  Created by Ben Scheirman on 3/31/17.
+//  Copyright © 2017 5calls. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class LocationButton : UIButton {
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        layer.borderColor = tintColor.cgColor
+        layer.borderWidth = 1
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        layer.cornerRadius = frame.size.height/2
+    }
+    
+    private var _highlited = false
+    override var isHighlighted: Bool {
+        get { return _highlited }
+        set {
+            _highlited = newValue
+            backgroundColor = isHighlighted ? tintColor : .white
+            titleLabel?.textColor = isHighlighted ? .white : tintColor
+            titleLabel?.textColor = isHighlighted ? .white : tintColor
+        }
+    }
+}
