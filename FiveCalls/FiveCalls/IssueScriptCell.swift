34f52d173432d708fb40f438706618ad21396c40
Nick O'Neill <nick.oneill@gmail.com>
2020-04-28 12:55:47 -0700

    Remove the copy warning... it causes a crash in certain cases and imho is not effective.

--- FiveCalls/FiveCalls/IssueScriptCell.swift
+++ FiveCalls/FiveCalls/IssueScriptCell.swift
@@ -9,23 +9,5 @@
 import UIKit
 
 class IssueScriptCell: UITableViewCell {
-
     @IBOutlet weak var scriptTextView: UITextView!
-    
-
-}
-
-class CopyWarningTextView: UITextView {
-    @IBOutlet weak var viewController: UIViewController?
-    
-    override func copy(_ sender: Any?) {
-        if !UserDefaults.standard.bool(forKey: UserDefaultsKey.hasWarnedAboutDangersOfCopying.rawValue) {
-            let alert = UIAlertController(title: R.string.localizable.thinkBeforeCopyingAlertTitle(), message: R.string.localizable.thinkBeforeCopyingAlertBody(), preferredStyle: .alert)
-            
-            alert.addAction(UIAlertAction(title:  R.string.localizable.okButtonTitle(), style: .cancel, handler: { action in }))
-            self.viewController?.present(alert, animated: true, completion: nil)
-            UserDefaults.standard.set(true, forKey: UserDefaultsKey.hasWarnedAboutDangersOfCopying.rawValue)
-        }
-        super.copy(sender)
-    }
 }
