24c349ca781d623b6215b5182e14905d4fb541aa
Nick O'Neill <nick.oneill@gmail.com>
2023-08-03 19:27:51 -0700

    added a contact image view

--- FiveCalls/FiveCalls/PreviewContacts.swift
+++ FiveCalls/FiveCalls/PreviewContacts.swift
@@ -9,7 +9,7 @@
 import Foundation
 
 extension Contact {
-    static let housePreviewContact = Contact(id: "1234", area: "US House", name: "Housy McHouseface", party: "Democrat", phone: "4155551212")
+    static let housePreviewContact = Contact(id: "1234", area: "US House", name: "Housy McHouseface", party: "Democrat", phone: "4155551212", photoURL: URL(string: "https://images.5calls.org/senate/256/S001227.jpg")!)
     static let senatePreviewContact1 = Contact(id: "12345", area: "US Senate", name: "Senatey McDefinitelyOld", party: "Democrat", phone: "4155551212")
     static let senatePreviewContact2 = Contact(id: "12346", area: "US Senate", name: "Senatey McShouldHaveRetired", party: "Democrat", phone: "4155551212")
 }
