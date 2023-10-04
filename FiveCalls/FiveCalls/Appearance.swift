4a0823b3bded50fc12b5598e95c2e2a0d328cbf7
Christopher Selin <christopher.selin@gmail.com>
2023-10-04 13:28:38 -0400

    fix page control tint colors

--- FiveCalls/FiveCalls/Appearance.swift
+++ FiveCalls/FiveCalls/Appearance.swift
@@ -20,6 +20,9 @@ enum Appearance {
     }
     
     static func swiftUISetup() {
+        let pageControlAppearance = UIPageControl.appearance()
+        pageControlAppearance.pageIndicatorTintColor = R.color.fivecallsLightBlue()
+        pageControlAppearance.currentPageIndicatorTintColor = R.color.fivecallsDarkBlue()
         UINavigationBar.appearance().backIndicatorImage = UIImage(systemName: "chevron.backward.circle.fill")
         UINavigationBar.appearance().backIndicatorTransitionMaskImage = UIImage(systemName: "chevron.backward.circle.fill")
         UIDatePicker.appearance().minuteInterval = 10
