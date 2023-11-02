f2d099a5ef45221d059c75207167146c5b5c38fa
Christopher Selin <christopher.selin@gmail.com>
2023-11-02 11:47:33 -0400

    temporary extra tabs for easier review

--- FiveCalls/FiveCalls/Dashboard.swift
+++ FiveCalls/FiveCalls/Dashboard.swift
@@ -18,82 +18,82 @@ struct Dashboard: View {
     @State var showAboutSheet = false
 
     var body: some View {
-            VStack(alignment: .leading, spacing: 10) {
-                HStack {
-                    Menu {
-                        Button(action: {
-                            showRemindersSheet.toggle()
-                        }, label: {
-                            Text(R.string.localizable.menuScheduledReminders())
-                        })
-                        Button(action: {
-                            showYourImpact.toggle()
-                        }, label: {
-                            Text(R.string.localizable.menuYourImpact())
-                        })
-                        Button(action: {
-                            showAboutSheet.toggle()
-                        }, label: {
-                            Text(R.string.localizable.menuAbout())
-                        })
-                    } label: {
-                        Image(.gear).renderingMode(.template).tint(Color.fivecallsDarkBlue)
-                    }
-                    .popoverTipIfApplicable(
-                        title: Text(R.string.localizable.menuTipTitle()),
-                        message: Text(R.string.localizable.menuTipMessage()))
-                    .sheet(isPresented: $showRemindersSheet) {
-                        ScheduleReminders()
-                    }
-                    .sheet(isPresented: $showYourImpact) {
-                        YourImpact()
-                    }
-                    .sheet(isPresented: $showAboutSheet) {
-                        AboutSheet()
-                    }
-                    
-                    LocationHeader(location: store.state.location, fetchingContacts: store.state.fetchingContacts)
-                        .padding(.bottom, 10)
-                        .onTapGesture {
-                            showLocationSheet.toggle()
+                VStack(alignment: .leading, spacing: 10) {
+                    HStack {
+                        Menu {
+                            Button(action: {
+                                showRemindersSheet.toggle()
+                            }, label: {
+                                Text(R.string.localizable.menuScheduledReminders())
+                            })
+                            Button(action: {
+                                showYourImpact.toggle()
+                            }, label: {
+                                Text(R.string.localizable.menuYourImpact())
+                            })
+                            Button(action: {
+                                showAboutSheet.toggle()
+                            }, label: {
+                                Text(R.string.localizable.menuAbout())
+                            })
+                        } label: {
+                            Image(.gear).renderingMode(.template).tint(Color.fivecallsDarkBlue)
                         }
-                        .sheet(isPresented: $showLocationSheet) {
-                            LocationSheet()
-                                .presentationDetents([.medium])
-                                .presentationDragIndicator(.visible)
-                                .padding(.top, 40)
-                            Spacer()
+                        .popoverTipIfApplicable(
+                            title: Text(R.string.localizable.menuTipTitle()),
+                            message: Text(R.string.localizable.menuTipMessage()))
+                        .sheet(isPresented: $showRemindersSheet) {
+                            ScheduleReminders()
                         }
-                    
-                    Image(.fivecallsStars)
-                }
-                .padding(.horizontal, 10)
-                .padding(.bottom, 10)
-                
-                Text(R.string.localizable.whatsImportantTitle())
-                    .font(.system(size: 20))
-                    .fontWeight(.semibold)
+                        .sheet(isPresented: $showYourImpact) {
+                            YourImpact()
+                        }
+                        .sheet(isPresented: $showAboutSheet) {
+                            AboutSheet()
+                        }
+                        
+                        LocationHeader(location: store.state.location, fetchingContacts: store.state.fetchingContacts)
+                            .padding(.bottom, 10)
+                            .onTapGesture {
+                                showLocationSheet.toggle()
+                            }
+                            .sheet(isPresented: $showLocationSheet) {
+                                LocationSheet()
+                                    .presentationDetents([.medium])
+                                    .presentationDragIndicator(.visible)
+                                    .padding(.top, 40)
+                                Spacer()
+                            }
+                        
+                        Image(.fivecallsStars)
+                    }
                     .padding(.horizontal, 10)
-                List(store.state.issues, selection: $selectedIssue) { issue in
-                    NavigationLink(value: issue) {
-                        IssueListItem(issue: issue, contacts: store.state.contacts)
+                    .padding(.bottom, 10)
+                    
+                    Text(R.string.localizable.whatsImportantTitle())
+                        .font(.system(size: 20))
+                        .fontWeight(.semibold)
+                        .padding(.horizontal, 10)
+                    List(store.state.issues, selection: $selectedIssue) { issue in
+                        NavigationLink(value: issue) {
+                            IssueListItem(issue: issue, contacts: store.state.contacts)
+                        }
+                    }
+                    .tint(Color.fivecallsLightBG)
+                    .listStyle(.plain)
+                }
+                .navigationBarHidden(true)
+                .onAppear() {
+                    //              TODO: refresh if issues are old too?
+                    if store.state.issues.isEmpty {
+                        store.dispatch(action: .FetchIssues)
+                    }
+                    
+                    if let location = store.state.location, store.state.contacts.isEmpty {
+                        store.dispatch(action: .FetchContacts(location))
                     }
                 }
-                .tint(Color.fivecallsLightBG)
-                .listStyle(.plain)
             }
-        .navigationBarHidden(true)
-        .onAppear() {
-            //              TODO: refresh if issues are old too?
-            if store.state.issues.isEmpty {
-                store.dispatch(action: .FetchIssues)
-            }
-            
-            if let location = store.state.location, store.state.contacts.isEmpty {
-                store.dispatch(action: .FetchContacts(location))
-            }
-        }
-    }
 }
 
 struct Dashboard_Previews: PreviewProvider {
