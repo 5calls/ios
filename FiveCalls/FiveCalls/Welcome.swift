b29f08cdf679ea99a05600229eb9055a86b31ff9
heathermh <heathermh@users.noreply.github.com>
2023-11-08 14:11:04 -0800

    Updated welcome screen to a newer style
    Updated layout to single screen, updated to be in line with apple standard designs
    Friendly for dark mode and dynamic text

--- FiveCalls/FiveCalls/Welcome.swift
+++ FiveCalls/FiveCalls/Welcome.swift
@@ -9,22 +9,113 @@
 import SwiftUI
 
 struct Welcome: View {
+    @Environment(\.dismiss) var dismiss
     @EnvironmentObject var store: Store
-    
+
     var onContinue: (() -> Void)?
-    
-    var body: some View {
-        VStack {
-            Image(.fivecallsLogotype)
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(width: 292)
-                .padding(.vertical, 24)
-            TabView {
-                Page1()
-                Page2(onContinue: onContinue)
+
+    struct CustomLabel: LabelStyle {
+        var spacing: Double = 0.0
+
+        func makeBody(configuration: Configuration) -> some View {
+            HStack(spacing: spacing) {
+                configuration.icon
+                configuration.title
             }
-            .tabViewStyle(.page)
+        }
+    }
+
+    var subMessage: String {
+        guard store.state.globalCallCount > 0 else {
+            return ""
+        }
+
+        return String(format: R.string.localizable.welcomePage2Calls(
+            StatsViewModel(numberOfCalls: store.state.globalCallCount).formattedNumberOfCalls)
+        )
+    }
+
+    var subMessageOpacity: Double {
+        store.state.globalCallCount > 0 ? 1 : 0
+    }
+
+    var body: some View {
+        ScrollView {
+            VStack {
+                Image(decorative: R.image.fivecallsLogotype)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 292)
+                    .padding(.vertical, 24)
+
+                VStack(alignment: .leading, spacing: 30) {
+                    Label {
+                        VStack(alignment: .leading) {
+                            Text(R.string.localizable.welcomePage1Title())
+                                .fontWeight(.heavy)
+                                .multilineTextAlignment(.leading)
+                            Text(R.string.localizable.welcomePage1Message())
+                                .multilineTextAlignment(.leading)
+                        }
+                        .accessibilityElement(children: .combine)
+                    } icon: {
+                        Image(systemName: "phone.badge.waveform")
+                            .symbolRenderingMode(.palette)
+                            .foregroundStyle(.fivecallsRed, .fivecallsDarkBlue)
+                    }
+                    .labelStyle(CustomLabel(spacing: 20))
+
+                    Label {
+                        VStack(alignment: .leading) {
+                            Text(R.string.localizable.welcomePage2Title())
+                                .fontWeight(.heavy)
+                            Text(R.string.localizable.welcomePage2Message())
+                        }
+                        .accessibilityElement(children: .combine)
+                    } icon: {
+                        Image(systemName: "goforward.5")
+                            .foregroundStyle(.fivecallsDarkBlue)
+                    }
+                    .labelStyle(CustomLabel(spacing: 20))
+
+                    Label {
+                        VStack(alignment: .leading) {
+                            Text(subMessage)
+                                .fontWeight(.heavy)
+                                .opacity(subMessageOpacity)
+                        }
+                        .accessibilityElement(children: .combine)
+                    } icon: {
+                        Image(systemName: "person.2")
+                            .opacity(subMessageOpacity)
+                            .symbolRenderingMode(.palette)
+                            .foregroundStyle(.fivecallsGreen, .fivecallsDarkBlue)
+                    }
+                    .labelStyle(CustomLabel(spacing: 12))
+                    Spacer()
+                    Button(action: {
+                        onContinue?()
+                        dismiss()
+                    }) {
+                        // TODO: update string if needed
+                        //          Text(R.string.localizable.welcomePage2ButtonTitle())
+                        Text("Continue")
+                            .foregroundColor(.white)
+                    }
+                    .padding()
+                    .frame(maxWidth: .infinity)
+                    .background {
+                        RoundedRectangle(cornerRadius: 6)
+                            .foregroundColor(.fivecallsDarkBlue)
+                    }
+                }
+                .padding(30)
+            }
+            .onAppear() {
+                if store.state.globalCallCount == 0 {
+                    store.dispatch(action: .FetchStats(nil))
+                }
+            }
         }
     }
 }
@@ -37,84 +128,8 @@ struct Welcome_Previews: PreviewProvider {
     }()
 
     static let previewStore = Store(state: previewState, middlewares: [appMiddleware()])
-   
+
     static var previews: some View {
         Welcome().environmentObject(previewStore)
-    }
-}
-
-struct Page1: View {
-    var body: some View {
-        VStack(alignment: .leading, spacing: 20) {
-            Text(R.string.localizable.welcomePage1Title())
-                .font(.title)
-                .textCase(.uppercase)
-                .foregroundStyle(.fivecallsDarkBlueText)
-            Text(R.string.localizable.welcomePage1Message())
-                .font(.headline)
-                .foregroundStyle(.fivecallsDarkBlueText)
-            Spacer()
-        }
-        .padding(EdgeInsets(top: 24, leading: 24, bottom: 20, trailing: 24))
-    }
-}
-
-struct Page2: View {
-    @Environment(\.dismiss) var dismiss
-    @EnvironmentObject var store: Store
-    
-    var onContinue: (() -> Void)?
-    
-    var subMessage: String {
-        guard store.state.globalCallCount > 0 else {
-            return ""
-        }
-        
-        return String(format: R.string.localizable.welcomePage2Calls(
-            StatsViewModel(numberOfCalls: store.state.globalCallCount).formattedNumberOfCalls)
-        )
-    }
-    
-    var subMessageOpacity: Double {
-        store.state.globalCallCount > 0 ? 1 : 0
-    }
-
-    var body: some View {
-        VStack(alignment: .leading, spacing: 10) {
-            Text(R.string.localizable.welcomePage2Title())
-                .font(.title)
-                .foregroundStyle(.fivecallsDarkBlueText)
-            Text(R.string.localizable.welcomePage2Message())
-                .font(.headline)
-                .foregroundStyle(.fivecallsDarkBlueText)
-            Text(subMessage)
-                .font(.headline)
-                .textCase(.uppercase)
-                .foregroundStyle(.fivecallsDarkBlueText)
-                .opacity(subMessageOpacity)
-                .animation(.easeIn, value: subMessageOpacity)
-            Spacer()
-            Button(action: {
-                onContinue?()
-                dismiss()
-            }) {
-                Text(R.string.localizable.welcomePage2ButtonTitle())
-                    .fontWeight(.semibold)
-                    .foregroundColor(.white)
-                    .font(.system(size: 30))
-            }
-            .padding()
-            .frame(maxWidth: .infinity, minHeight: 73)
-            .background {
-                RoundedRectangle(cornerRadius: 6)
-                    .foregroundColor(Color.fivecallsDarkBlue)
-            }
-        }
-        .padding(EdgeInsets(top: 24, leading: 24, bottom: 48, trailing: 24))
-        .onAppear() {
-            if store.state.globalCallCount == 0 {
-                store.dispatch(action: .FetchStats(nil))
-            }
-        }
     }
 }
