b9c36870e6b9f0bc409521731a54f4d4bb5d8f44
heathermh <heathermh@users.noreply.github.com>
2023-11-10 15:21:55 -0800

    Updates to layout for welcome view
    Switched to grid
    Adjusted size/color of symbols

--- FiveCalls/FiveCalls/Welcome.swift
+++ FiveCalls/FiveCalls/Welcome.swift
@@ -14,23 +14,12 @@ struct Welcome: View {
 
     var onContinue: (() -> Void)?
 
-    struct CustomLabel: LabelStyle {
-        var spacing: Double = 0.0
-
-        func makeBody(configuration: Configuration) -> some View {
-            HStack(spacing: spacing) {
-                configuration.icon
-                configuration.title
-            }
-        }
-    }
-
     var subMessage: String {
         guard store.state.globalCallCount > 0 else {
             return ""
         }
 
-        return String(format: R.string.localizable.welcomePage2Calls(
+        return String(format: R.string.localizable.welcomeSection3Calls(
             StatsViewModel(numberOfCalls: store.state.globalCallCount).formattedNumberOfCalls)
         )
     }
@@ -41,81 +30,72 @@ struct Welcome: View {
 
     var body: some View {
         ScrollView {
-            VStack {
+            Grid(verticalSpacing: 30) {
                 Image(decorative: R.image.fivecallsLogotype)
                     .resizable()
                     .aspectRatio(contentMode: .fit)
                     .frame(width: 292)
                     .padding(.vertical, 24)
-
-                VStack(alignment: .leading, spacing: 30) {
-                    Label {
-                        VStack(alignment: .leading) {
-                            Text(R.string.localizable.welcomePage1Title())
-                                .fontWeight(.heavy)
-                                .multilineTextAlignment(.leading)
-                            Text(R.string.localizable.welcomePage1Message())
-                                .multilineTextAlignment(.leading)
-                        }
-                        .accessibilityElement(children: .combine)
-                    } icon: {
-                        Image(systemName: "phone.badge.waveform")
-                            .symbolRenderingMode(.palette)
-                            .foregroundStyle(.fivecallsRed, .fivecallsDarkBlue)
+                GridRow() {
+                    Image(systemName: "phone.badge.waveform")
+                        .symbolRenderingMode(.palette)
+                        .foregroundStyle(.red, .blue)
+                        .font(.title)
+                        .accessibilityHidden(true)
+                    VStack(alignment: .leading) {
+                        Text(R.string.localizable.welcomeSection1Title())
+                            .fontWeight(.heavy)
+                        Text(R.string.localizable.welcomeSection1Message())
                     }
-                    .labelStyle(CustomLabel(spacing: 20))
-
-                    Label {
-                        VStack(alignment: .leading) {
-                            Text(R.string.localizable.welcomePage2Title())
-                                .fontWeight(.heavy)
-                            Text(R.string.localizable.welcomePage2Message())
-                        }
-                        .accessibilityElement(children: .combine)
-                    } icon: {
-                        Image(systemName: "goforward.5")
-                            .foregroundStyle(.fivecallsDarkBlue)
-                    }
-                    .labelStyle(CustomLabel(spacing: 20))
-
-                    Label {
-                        VStack(alignment: .leading) {
-                            Text(subMessage)
-                                .fontWeight(.heavy)
-                                .opacity(subMessageOpacity)
-                        }
-                        .accessibilityElement(children: .combine)
-                    } icon: {
-                        Image(systemName: "person.2")
-                            .opacity(subMessageOpacity)
-                            .symbolRenderingMode(.palette)
-                            .foregroundStyle(.fivecallsGreen, .fivecallsDarkBlue)
-                    }
-                    .labelStyle(CustomLabel(spacing: 12))
-                    Spacer()
-                    Button(action: {
-                        onContinue?()
-                        dismiss()
-                    }) {
-                        // TODO: update string if needed
-                        //          Text(R.string.localizable.welcomePage2ButtonTitle())
-                        Text("Continue")
-                            .foregroundColor(.white)
-                    }
-                    .padding()
-                    .frame(maxWidth: .infinity)
-                    .background {
-                        RoundedRectangle(cornerRadius: 6)
-                            .foregroundColor(.fivecallsDarkBlue)
-                    }
+                    .accessibilityElement(children: .combine)
                 }
-                .padding(30)
+                GridRow() {
+                    Image(systemName: "goforward.5")
+                        .foregroundStyle(.blue)
+                        .font(.title)
+                        .accessibilityHidden(true)
+                    VStack(alignment: .leading) {
+                        Text(R.string.localizable.welcomeSection2Title())
+                            .fontWeight(.heavy)
+                        Text(R.string.localizable.welcomeSection2Message())
+                    }
+                    .accessibilityElement(children: .combine)
+                }
+                GridRow() {
+                    Image(systemName: "person.2")
+                        .opacity(subMessageOpacity)
+                        .symbolRenderingMode(.palette)
+                        .foregroundStyle(.blue, .red)
+                        .font(.title)
+                        .accessibilityHidden(true)
+                    Text(subMessage)
+                        .fontWeight(.heavy)
+                        .opacity(subMessageOpacity)
+                        .gridColumnAlignment(.leading)
+                }
+                Spacer()
+                Spacer()
+                Button(action: {
+                    onContinue?()
+                    dismiss()
+                }) {
+                    Text(R.string.localizable.welcomeButtonTitle())
+                        .foregroundColor(.white)
+                }
+                .padding()
+                .frame(maxWidth: .infinity)
+                .background {
+                    RoundedRectangle(cornerRadius: 6)
+                        .foregroundColor(.blue)
+                }
             }
             .onAppear() {
                 if store.state.globalCallCount == 0 {
                     store.dispatch(action: .FetchStats(nil))
                 }
             }
+            .frame(maxWidth: .infinity)
+            .padding(30)
         }
     }
 }
