98ed561974aa6dac86a29c8b093e6b463ed7f910
Nick O'Neill <nick.oneill@gmail.com>
2023-11-18 13:56:39 -0800

    remove old tracking, only track events we care about

--- FiveCalls/FiveCalls/AppDelegate.swift
+++ FiveCalls/FiveCalls/AppDelegate.swift
@@ -57,7 +57,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         }
         // sets a string like 'usps-postal-service-covid-funding' that we can use when issues are loaded
         UserDefaults.standard.set(incomingURL.lastPathComponent, forKey: UserDefaultsKey.selectIssuePath.rawValue)
-        AnalyticsManager.shared.trackEventOld(withName: "Info: Enter from Universal Link", andProperties: ["issue-slug": incomingURL.lastPathComponent])
 
         return true
     }
