d9e28a8019035c04969f936249fb8284718f0c6a
Chris Selin <810263+cselin@users.noreply.github.com>
2023-12-20 12:25:38 -0500

    Add phone number call and copy functionality (#397)
    * long tap copy (doesn’t work with field offices)
    
    * Revert "long tap copy (doesn’t work with field offices)"
    
    This reverts commit f15de66dc3101a6c7185fc744a096a6b3699211d.
    
    * use confirmation dialog
    
    * Revert "use confirmation dialog"
    
    This reverts commit 4298a5e9381bf6d32736dfb176b38b74354a6416.
    
    * Revert "Revert "long tap copy (doesn’t work with field offices)""
    
    This reverts commit 24dbb5b16d819b66271c7328be9e358cab482ec7.
    
    * use control group and add accessibility hints
    
    * fixed voiceover
    
    * conflict resolution

--- FiveCalls/FiveCalls/IssueContactDetail.swift
+++ FiveCalls/FiveCalls/IssueContactDetail.swift
@@ -26,6 +26,10 @@ struct IssueContactDetail: View {
     var issueMarkdown: AttributedString {
         return issue.markdownIssueScript(contact: currentContact, location: store.state.location)
     }
+
+    @State private var copiedPhoneNumber: String?
+    @AccessibilityFocusState private var isCopiedPhoneNumberFocused: Bool
+
     
     var body: some View {
         ScrollView {
@@ -45,30 +49,103 @@ struct IssueContactDetail: View {
                 VStack(alignment: .trailing) {
                     HStack {
                         Spacer()
+                        
+                        if let copiedPhoneNumber {
+                            Text(R.string.localizable.copiedPhone(copiedPhoneNumber))
+                                .bold()
+                                .font(.footnote)
+                                .multilineTextAlignment(.center)
+                                .accessibilityFocused($isCopiedPhoneNumberFocused)
+                                .accessibilityLabel(R.string.localizable.a11yCopiedPhoneNumber())
+                            Spacer()
+                        }
+                        
                         Text(currentContact.phone)
                             .font(.title)
                             .fontWeight(.semibold)
                             .foregroundColor(Color.fivecallsDarkBlueText)
-                        Menu {
-                            ForEach(currentContact.fieldOffices) { office in
-                                Section(office.city) {
-                                    Button{ } label: {
-                                        VStack {
-                                            Text(office.phone)
+                            .onTapGesture {
+                                self.call(phoneNumber: currentContact.phone)
+                            }
+                            .onLongPressGesture(minimumDuration: 1.0) {
+                                UIPasteboard.general.string = currentContact.phone
+                                
+                                withAnimation {
+                                    copiedPhoneNumber = currentContact.phone
+                                    if UIAccessibility.isVoiceOverRunning {
+                                        DispatchQueue.main.asyncAfter(wallDeadline: .now() + 0.5) {
+                                            isCopiedPhoneNumberFocused = true
                                         }
                                     }
                                 }
+                                
+                                DispatchQueue.main.asyncAfter(wallDeadline: .now() + 3) {
+                                    withAnimation {
+                                        isCopiedPhoneNumberFocused = false
+                                        copiedPhoneNumber = nil
+                                    }
+                                }
                             }
-                        } label: {
-                            Image(systemName: "ellipsis.circle")
-                                .font(.title2)
-                                .foregroundColor(Color.fivecallsDarkBlue)
-                                .padding(.leading, 4)
+                            .accessibilityAddTraits(.isButton)
+                            .accessibilityHint(R.string.localizable.a11yPhoneCallCopyHint())
+                        
+                        if currentContact.fieldOffices.count > 1 {
+                            Menu {
+                                ForEach(currentContact.fieldOffices) { office in
+                                    Section {
+                                        Text(office.city)
+                                    }
+                                    
+                                    ControlGroup {
+                                        Button {
+                                            self.call(phoneNumber: office.phone)
+                                        } label: {
+                                            HStack {
+                                                Image(systemName: "phone")
+                                                Text(office.phone)
+                                            }
+                                        }
+                                        .accessibilityLabel(R.string.localizable.a11yCallPhoneNumber(office.phone))
+                                        .accessibilityHint(R.string.localizable.a11yPhoneCallHint())
+                                        
+                                        Button {
+                                            UIPasteboard.general.string = office.phone
+                                            withAnimation {
+                                                copiedPhoneNumber = office.phone
+                                                if UIAccessibility.isVoiceOverRunning {
+                                                    isCopiedPhoneNumberFocused = true
+                                                }
+                                            }
+                                            
+                                            DispatchQueue.main.asyncAfter(wallDeadline: .now() + 3) {
+                                                withAnimation {
+                                                    isCopiedPhoneNumberFocused = false
+                                                    copiedPhoneNumber = nil
+                                                }
+                                            }
+                                        } label: {
+                                            HStack {
+                                                Image(systemName: "doc.on.doc")
+                                                Text(R.string.localizable.copy())
+                                            }
+                                        }
+                                        .accessibilityLabel(R.string.localizable.a11yCopyPhoneNumber(office.phone))
+                                        .accessibilityHint(R.string.localizable.a11yPhoneCopyHint())
+                                    }
+                                }
+                            } label: {
+                                Image(systemName: "ellipsis.circle")
+                                    .font(.title2)
+                                    .foregroundColor(Color.fivecallsDarkBlue)
+                                    .padding(.leading, 4)
+                            }
                         }
                     }
                 }.padding(.bottom)
+                
                 Text(issueMarkdown)
                     .padding(.bottom)
+                
                 if remainingContacts.count > 1 {
                     NavigationLink(value: IssueDetailNavModel(issue: issue, contacts: nextContacts)) {
                         OutcomesView(outcomes: issue.outcomeModels, report: { outcome in
@@ -91,6 +168,13 @@ struct IssueContactDetail: View {
             }.padding(.horizontal)
         }.navigationBarHidden(true)
         .clipped()
+    }
+    
+    private func call(phoneNumber: String) {
+        let telephone = "tel://"
+        let formattedString = telephone + phoneNumber
+        guard let url = URL(string: formattedString) else { return }
+        UIApplication.shared.open(url)
     }
 }
 
