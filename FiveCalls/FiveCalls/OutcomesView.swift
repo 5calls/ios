765ce0ec63613375ad0b042344d1a475a33123d1
heathermh <heathermh@users.noreply.github.com>
2024-01-04 15:37:57 -0800

    .onTap and NavLink not playing nice together for VO, removed NavLInk and it works
    Updating router is enough to trigger the navigation and doesn't conflict w/ VO
    Adjusted accessbility for PrimaryButton a bit so it works in different places

--- FiveCalls/FiveCalls/OutcomesView.swift
+++ FiveCalls/FiveCalls/OutcomesView.swift
@@ -16,35 +16,17 @@ struct OutcomesView<T: IssueNavModel>: View {
     var body: some View {
         LazyVGrid(columns: [GridItem(.flexible()),GridItem(.flexible())]) {
             ForEach(outcomes) { outcome in
-
-//                NavigationLink(value: IssueDetailNavModel(issue: issue, contacts: contacts)) {
-//                    PrimaryButton(title: R.string.localizable.seeScript(), systemImageName: "megaphone.fill")
-//                }
-
-
-
-                NavigationLink(value: value) {
-                    PrimaryButton(title: outcome.label.capitalized,
+                PrimaryButton(title: outcome.label.capitalized,
                                   systemImageName: "megaphone.fill")
-
-                    // TODO: .onTap and .simultaneous not being triggered with voiceover?
-//                    .accessibilityAddTraits(.isButton)
-//                    .simultaneousGesture(TapGesture().onEnded {
-//                        print("tapped button for \(outcome.label)")
-//                        report(outcome)
-//                    })
-                    .onTapGesture {
-                        print("tapped button for \(outcome.label)")
-                        report(outcome)
-                    }
+                .accessibilityAddTraits(.isButton)
+                .onTapGesture {
+                    report(outcome)
                 }
             }
         }
-
     }
 }
 
 #Preview {
     OutcomesView(value: IssueDetailNavModel(issue: .basicPreviewIssue, contacts: [.housePreviewContact, .senatePreviewContact1, .senatePreviewContact2]), outcomes: [Outcome(label: "OK", status: "ok"),Outcome(label: "No", status: "no"),Outcome(label: "Maybe", status: "maybe")], report: { _ in })
-//        .padding()
 }
