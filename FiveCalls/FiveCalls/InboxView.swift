fe7942bffaadaae8847d8b648e798987564eee73
Nick O'Neill <nick.oneill@gmail.com>
2024-07-03 12:46:01 -0700

    no-location view for inbox

--- FiveCalls/FiveCalls/InboxView.swift
+++ FiveCalls/FiveCalls/InboxView.swift
@@ -35,65 +35,84 @@ struct InboxView: View {
                 .padding(.horizontal, 10)
                 .padding(.bottom, 10)
 
-            ScrollView {
-                HStack {
-                    Text("Your National Reps")
-                        .font(.body)
-                        .fontWeight(.medium)
-                        .foregroundStyle(.secondary)
-                    Spacer()
-                }
-                
-                VStack(spacing: 0) {
-                    ForEach(contacts.numbered(), id: \.element.id) { contact in
-                        ContactListItem(contact: contact.element, showComplete: false)
-                    }
-                }
-
-                HStack {
-                    Text("Recent votes")
-                        .font(.body)
-                        .fontWeight(.medium)
-                        .foregroundStyle(.secondary)
-                    Spacer()
-                }.padding(.top, 10)
-
-                if showPushPrompt {
-                    VStack {
-                        PrimaryButton(title: "Send me important votes")
-                            .onTapGesture {
-                                OneSignal.promptForPushNotifications { success in
-                                    Task {
-                                        await updateNotificationStatus()
-                                    }
-                                }
-                            }
-                        Text("Get notified how your rep votes on important issues")
-                            .font(.caption)
-                            .fontWeight(.medium)
-                            .frame(maxWidth: .infinity, alignment: .center)
-                    }.padding(.vertical, 10)
-                }
-                
+            if store.state.contacts.isEmpty {
                 VStack {
-                    ForEach(store.state.repMessages, id: \.id) { message in
-                        if let repID = message.repID, let contact = self.contactForID(contactId: repID) {
-                            ContactInboxVote(contact: contact, message: message)
-                                .padding(.bottom, 6)
-                                .onTapGesture{
-                                    store.dispatch(action: .SelectMessage(message))
-                                    detailPresented = true
-                                }
-                        } else if let _ = message.imageURL {
-                            GenericInboxVote(message: message)
-                                .onTapGesture{
-                                    store.dispatch(action: .SelectMessage(message))
-                                    detailPresented = true
-                                }
+                    Spacer()
+                    HStack {
+                        Spacer()
+                        Image(systemName: "arrowshape.up.circle")
+                            .font(.title)
+                            .foregroundColor(.secondary)
+                            .padding(.trailing, 4)
+                        Text("Set your location\rto see your reps")
+                            .font(.title2)
+                            .lineLimit(2)
+                            .foregroundColor(.secondary)
+                        Spacer()
+                    }
+                    Spacer()
+                }
+            } else {
+                ScrollView {
+                    HStack {
+                        Text("Your National Reps")
+                            .font(.body)
+                            .fontWeight(.medium)
+                            .foregroundStyle(.secondary)
+                        Spacer()
+                    }
+                    
+                    VStack(spacing: 0) {
+                        ForEach(contacts.numbered(), id: \.element.id) { contact in
+                            ContactListItem(contact: contact.element, showComplete: false)
                         }
                     }
-                }.padding(4)
-            }.scrollIndicators(.hidden)
+
+                    HStack {
+                        Text("Recent votes")
+                            .font(.body)
+                            .fontWeight(.medium)
+                            .foregroundStyle(.secondary)
+                        Spacer()
+                    }.padding(.top, 10)
+
+                    if showPushPrompt {
+                        VStack {
+                            PrimaryButton(title: "Send me important votes")
+                                .onTapGesture {
+                                    OneSignal.promptForPushNotifications { success in
+                                        Task {
+                                            await updateNotificationStatus()
+                                        }
+                                    }
+                                }
+                            Text("Get notified how your rep votes on important issues")
+                                .font(.caption)
+                                .fontWeight(.medium)
+                                .frame(maxWidth: .infinity, alignment: .center)
+                        }.padding(.vertical, 10)
+                    }
+                    
+                    VStack {
+                        ForEach(store.state.repMessages, id: \.id) { message in
+                            if let repID = message.repID, let contact = self.contactForID(contactId: repID) {
+                                ContactInboxVote(contact: contact, message: message)
+                                    .padding(.bottom, 6)
+                                    .onTapGesture{
+                                        store.dispatch(action: .SelectMessage(message))
+                                        detailPresented = true
+                                    }
+                            } else if let _ = message.imageURL {
+                                GenericInboxVote(message: message)
+                                    .onTapGesture{
+                                        store.dispatch(action: .SelectMessage(message))
+                                        detailPresented = true
+                                    }
+                            }
+                        }
+                    }.padding(4)
+                }.scrollIndicators(.hidden)
+            }
         }.padding(.horizontal, 16)
             .sheet(isPresented: $detailPresented) {
                 InboxDetail()
@@ -110,9 +129,9 @@ struct InboxView: View {
     let previewState = {
         let state = AppState()
         state.contacts = [
-            Contact.housePreviewContact,
-            Contact.senatePreviewContact1,
-            Contact.senatePreviewContact2
+//            Contact.housePreviewContact,
+//            Contact.senatePreviewContact1,
+//            Contact.senatePreviewContact2
         ]
         state.repMessages = [
             InboxMessage.houseMessage,
