ce5b962dc2222a747de23534541a3a88433f8998
Nick O'Neill <nick.oneill@gmail.com>
2025-08-22 15:23:53 -0700

    move state badge around, fix double state repping

--- FiveCalls/FiveCalls/PreviewContacts.swift
+++ FiveCalls/FiveCalls/PreviewContacts.swift
@@ -13,5 +13,7 @@ extension Contact {
     static let senatePreviewContact1 = Contact(id: "12345", area: "US Senate", name: "Senatey McDefinitelyOld", party: "Democrat", phone: "415-555-1212")
     static let senatePreviewContact2 = Contact(id: "12346", area: "US Senate", name: "Senatey McShouldHaveRetired", party: "Democrat", phone: "415-555-1212")
     static let weirdShapeImagePreviewContact = Contact(id: "12347", area: "US Senate", name: "Senatey McShouldHaveRetired", party: "Democrat", phone: "415-555-1212", photoURL: URL(string: "https://www.assembly.ca.gov/sites/assembly.ca.gov/files/memberphotos/ad17_haney.jpg")!)
+    static let governorPreviewContact = Contact(id: "12348", area: "Governor", name: "Govy McGovern", party: "Democrat", phone: "415-555-1212")
+    static let agPreviewContact = Contact(id: "12349", area: "AttorneyGeneral", name: "AG McRanOutOfIdeas", party: "Democrat", phone: "415-555-1212")
     static let unknownMayorPreviewContact = Contact(id: "1234", area: "Mayor", name: "Mayor McMayorface", party: "Democrat", phone: "415-555-1212", photoURL: URL(string: "https://images.5calls.org/senate/256/S001227.jpg")!, fieldOffices: [AreaOffice(city: "San Francisco", phone: "415-513-1111"),AreaOffice(city: "San Diego", phone: "415-513-2222")])
 }
